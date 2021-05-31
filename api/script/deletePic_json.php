88ff06f198d4e0dfc47be667b0655f3c689ade28
Maxime GROSSET <grosset.maxime@gmail.com>
2021-05-31 19:45:26 +0200

    Fix deletePic script

--- api/script/deletePic_json.php
+++ api/script/deletePic_json.php
@@ -44,8 +44,8 @@ $request_body = file_get_contents('php://input');
 $data = json_decode($request_body);
 
 $picPath = !empty($data->picPath) ? trim($data->picPath) : '';
-$continueIfNotExist = !empty($data->continueIfNotExist) ? !!$data->continueIfNotExist) : false;
-$deleteOnlyFromBdd = !empty($data->deleteOnlyFromBdd) ? !!$data->deleteOnlyFromBdd) : false;
+$continueIfNotExist = !empty($data->continueIfNotExist) ? !!$data->continueIfNotExist : false;
+$deleteOnlyFromBdd = !empty($data->deleteOnlyFromBdd) ? !!$data->deleteOnlyFromBdd : false;
 
 if (empty($picPath)) {
     $picPath = trim($_POST['picPath']) ? trim($_POST['picPath']) : '';
